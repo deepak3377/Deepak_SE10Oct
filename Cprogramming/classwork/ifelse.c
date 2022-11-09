5bc057ffe99c87b2cad84b5060c77fad472ada4b
deepak3377 <115608336+deepak3377@users.noreply.github.com>
2022-11-09 17:39:24 +0530

    Create ifelse.c

---
+++ Cprogramming/classwork/ifelse.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void main()
+{
+	int a;
+	
+	printf("\nEnter A : ");
+	scanf("%d",&a);
+	
+	if(a>0)
+	{
+		printf("\n is +v",a);
+	}
+
+	
+    else
+	{
+		printf("\n is -ve" ,a);
+	}	
+	
+	
+}
