09083e123bfea3003d988d806a75bedb97c0efe2
deepak3377 <115608336+deepak3377@users.noreply.github.com>
2022-11-25 17:32:34 +0530

    Create funcdemo.c

---
+++ Cprogramming/classwork/funcdemo.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+void func1()
+{
+	printf("\nthis is a function 1");
+	
+}
+
+void func2()
+{
+	printf("\nthis is a function 2");
+	
+}
+
+void func3()
+{
+	printf("\nthis is a function 3");
+}
+
+
+
+void main()
+
+{
+	func1();
+	func2();
+	func3();
+}
