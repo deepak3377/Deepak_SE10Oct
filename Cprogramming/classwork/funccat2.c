5eac817d7bd82deaded5bb7b539ca5745b02e039
deepak3377 <115608336+deepak3377@users.noreply.github.com>
2022-11-25 17:32:12 +0530

    Create funccat2.c

---
+++ Cprogramming/classwork/funccat2.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+void evenOdd(int n1)
+{
+	if(n1%2==0)
+	{
+	printf("\nn1 is even");
+    }
+    
+    else
+	
+	{
+		printf("\nIt is Odd");
+		
+	 } 
+    
+    
+	
+	
+}
+
+
+
+
+
+void main()
+
+
+{
+	
+	int a =51;
+	evenOdd(a);
+}
