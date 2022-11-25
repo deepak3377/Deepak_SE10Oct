777a630fa88b8cfc5c32ddcacb5f65437cc76507
deepak3377 <115608336+deepak3377@users.noreply.github.com>
2022-11-25 17:32:22 +0530

    Create funcCat3.c

---
+++ Cprogramming/classwork/funcCat3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int add(int a1,int b1)
+
+{
+	return a1+b1;
+
+	
+}
+
+
+
+
+void main()
+{
+	int a,b;
+	printf("%d",add(a,b));
+	printf("\nEnter A & B :");
+	scanf("%d%d",&a,&b);
+
+	
+}
