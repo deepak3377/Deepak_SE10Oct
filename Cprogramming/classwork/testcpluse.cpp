40083b8d5d50fecf7ca6ce71b08eefe860edc3fa
deepak3377 <115608336+deepak3377@users.noreply.github.com>
2022-11-25 17:33:21 +0530

    Create testcpluse.cpp

---
+++ Cprogramming/classwork/testcpluse.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a=54,b=6;
+	cout<<"\nWelcome to c++ Programing\n";
+	cout<<"\nA = "<<a<<"\nB = "<<b;
+	//cout<<"nB = "<<b;
+	
+	
+	return 0;
+}
