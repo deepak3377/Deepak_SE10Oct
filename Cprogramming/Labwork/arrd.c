b6103da3deedfb315c55fa95ecf3ccd4e1fb4a32
deepak3377 <115608336+deepak3377@users.noreply.github.com>
2022-11-25 17:33:44 +0530

    Create arrd.c

---
+++ Cprogramming/Labwork/arrd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main()
+
+{
+	int mars[5] = {53,33,77,69,35};
+	
+	printf("%d\t",mars[0]);
+	printf("%d\t",mars[1]);
+	printf("%d\t",mars[2]);
+	printf("%d\t",mars[3]);
+	printf("%d\t",mars[4]);
+	
+}
