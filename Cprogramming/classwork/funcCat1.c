d4913db4d3578c9f082213158d2255fddbea1f11
deepak3377 <115608336+deepak3377@users.noreply.github.com>
2022-11-25 17:32:05 +0530

    Create funcCat1.c

---
+++ Cprogramming/classwork/funcCat1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+
+void add()
+{
+	int a,b;
+	printf("\nEnter A & B : ");
+	scanf("%d%d",&a,&b);
+	printf("\nAddition : %d",(a+b));
+	
+}
+
+
+
+void main()
+{
+	
+	add();
+	
+	
+}
