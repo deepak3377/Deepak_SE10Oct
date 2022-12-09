2e6e7403042dc5b5dd41e2fb38106cf4fca75236
deepak3377 <115608336+deepak3377@users.noreply.github.com>
2022-12-09 08:45:42 +0530

    Create testcpluse1.cpp

---
+++ CPP_programmin/Classwork/testcpluse1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+int main()
+{
+	int a=4;
+	int b=5;
+	
+	cout<<"A = "<<a<<",B"<<b;
+	
+	cout<<"\n Addition : "<<(a+b);
+	cout<<"\nSubtraction : "<<(a-b);
+	cout<<"\nMultiplication : "<<(a*b);
+	printf("\nDivision : %.2f",((float)a/b));
+	//cout<<"\nDvision : "<<(a/b);
+	
+	return 0;
+	
+	
+}
