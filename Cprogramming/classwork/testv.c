c7ccc8f8574e39528297f27140e57e0c088cfbe5
deepak3377 <115608336+deepak3377@users.noreply.github.com>
2022-12-12 08:49:14 +0530

    Update testv.c

--- Cprogramming/classwork/testv.c
+++ Cprogramming/classwork/testv.c
@@ -2,19 +2,5 @@
 void main()
 
 {
-	char name[10];
-	int age;
-	float salary;
-	
-	printf("\nEnter Name : ");
-	//scanf("%s",&name);
-	gets(name);
-	printf("Enter Age :");
-	scanf("%d",&age);
-	printf("Enter Salary :");
-	scanf("%f",&salary);
-	
-	printf("\nName : %s",name);
-	printf("\nAge : %d",age);
-	printf("\nSalary : %.2f",salary);
+	printf("Name      : Deepak Varma\nBirthdate : 25-09-1999\nAge       : 23\nAddress   : Ahmedabad");
 }
