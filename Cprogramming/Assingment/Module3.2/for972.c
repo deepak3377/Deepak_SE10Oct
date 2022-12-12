96430243e92b61e65a88f3a1632bca0ca7ac3de1
deepak3377 <115608336+deepak3377@users.noreply.github.com>
2022-12-12 08:46:31 +0530

    Create for972.c

---
+++ Cprogramming/Assingment/Module3.2/for972.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+
+{
+	int i,num,odd_sum=0,even_sum=0;
+	for(i=1;i<=10;i++)
+	
+	{
+	printf("Enter the value : ");
+	scanf("%d",&num);
+    }
+	
+	
+		if(i%2 == 0)
+		{
+			printf("%d",i);
+		}
+	
+	
+	
+}
