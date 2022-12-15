9db872ffc91ca93fa3d61401c56dc77de1a01f56
deepak3377 <115608336+deepak3377@users.noreply.github.com>
2022-12-15 22:29:48 +0530

    Create areaofrec.c

---
+++ Cprogramming/Assingment/Module3.1/areaofrec.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+
+void main()
+
+{
+	int i,b,r,c,t;
+	int Area;
+	
+	printf("\nEnter Lenght : ");
+	scanf("%d",&i);
+	printf("\nEnter Bradth : ");
+	scanf("%d",&b);
+	
+	Area=i*b;
+	
+	printf("\nArea of Rectanle is %d",Area);
+{
+		
+	float pi=3.14,r,area;
+	{
+		printf("\n\nEnter the value : ");
+		scanf("%f",&r);
+	}
+	
+	area=pi*r*r;
+	{
+		printf("\nArea of Circle is : %f",area);
+		
+	}
+	
+	{
+		float area,h,b;
+		{
+			printf("\n\n Enter the height : ");
+			scanf("%f",&h);
+			printf("\nEnter the breadth :");
+			scanf("%f",&b);
+		}
+		
+		area=0.5*h*b;
+		{
+			printf("\nArea of Triangle is : %f",area);
+		}
+	}
+}
+}
