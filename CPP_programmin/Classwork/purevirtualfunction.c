10b4068e92a6ce698250d00e71b5c964d94208a6
deepak3377 <115608336+deepak3377@users.noreply.github.com>
2022-12-15 22:29:31 +0530

    Create purevirtualfunction.c

---
+++ CPP_programmin/Classwork/purevirtualfunction.c
@@ -0,0 +1,36 @@
+#include<iostream>
+
+using namespace std;
+
+class Demo{
+	public:
+		void f1();
+		{
+			cout<<"\nNon pure Virtual function in class Demo";
+		}
+		
+		virtual void f2();
+};
+
+class Sample : public Demo{
+	public:
+		void f3()
+		{
+			cout<<"\nNon pure Virtual Function in class sample";
+			
+		}
+		
+		void f2()
+		{
+			cout<<"\nPure Virtual Function in class Sample"
+		}
+};
+
+int main()
+{
+	Sample obj1;
+	obj1.f1();
+	obj1.f2();
+	obj1.f3();
+	return 0;
+}
