837700015065a496a70761b066b8be60ecdc63f7
deepak3377 <115608336+deepak3377@users.noreply.github.com>
2023-04-30 16:06:26 +0530

    Create Funcodd.py

---
+++ Python/LabWork/Funcodd.py
@@ -0,0 +1,16 @@
+def evend():
+    num = int(input("Enter Number"))
+    if num %2 == 0:
+        print(num,"Is Even")
+    else:
+        print(num,"Is Odd")
+
+def pove():
+    num = int(input("Enter Number : "))
+    if num >0:
+        print(num,"+ve")
+    else:
+        print(num,"is -ve")   
+
+evend()
+pove()                     
